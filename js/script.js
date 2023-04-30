a6baa5285c10e843c99dbffb4a67a6946abcc16e
Devon <Devon.smith017@gmail.com>
2023-04-30 17:15:59 -0500

    Add validation for email and phone number inputs.

--- js/script.js
+++ js/script.js
@@ -1,40 +1,71 @@
 const firstName = document.querySelector('#first-name');
 const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const phone = document.querySelector('#phone');
 const firstNameError = document.querySelector('#first-name + span.error');
 const lastNameError = document.querySelector('#last-name + span.error');
+const emailError = document.querySelector('#email + span.error');
+const phoneError = document.querySelector('#phone + span.error');
 const form = document.querySelector('form');
 
 firstName.addEventListener('input', checkFirstName);
 lastName.addEventListener('input', checkLastName);
+email.addEventListener('input', checkEmail);
+phone.addEventListener('input', checkPhoneNumber);
 
 
-form.addEventListener('submit', (e)=>{
-  if (!firstName.validity.valid){
+form.addEventListener('submit', (e) => {
+  if (!firstName.validity.valid) {
     checkFirstName();
     e.preventDefault();
   }
-  if (!lastName.validity.valid){
+  if (!lastName.validity.valid) {
     checkLastName();
+    e.preventDefault();
+  }
+  if (!email.validity.typeMismatch){
+    checkEmail();
+    e.preventDefault();
+  }
+  if (!phone.validity.typeMismatch){
+    checkPhoneNumber();
     e.preventDefault();
   }
   form.className = 'submit';
 })
 
-function checkFirstName (){
-  if (firstName.validity.valid){
+function checkFirstName() {
+  if (firstName.validity.valid) {
     firstNameError.textContent = '';
-    firstNameError.className = 'error';
-  } else if (firstName.validity.valueMissing){
+  } else if (firstName.validity.valueMissing) {
     firstNameError.textContent = '*Please enter your first name';
-    firstNameError.className = 'error active';
   }
 }
-function checkLastName (){
-  if (lastName.validity.valid){
+
+function checkLastName() {
+  if (lastName.validity.valid) {
     lastNameError.textContent = '';
-    lastNameError.className = 'error';
-  } else if (lastName.validity.valueMissing){
+  } else if (lastName.validity.valueMissing) {
     lastNameError.textContent = '*Please enter your last name';
-    lastNameError.className = 'error active';
+  }
+}
+
+function checkEmail(){
+  if (email.validity.typeMismatch){
+    emailError.textContent = '*Format: abc@example.com';
+  } else if(email.validity.valueMissing){
+    emailError.textContent = '*Please enter your email address';
+  } else {
+    emailError.textContent = '';
+  }
+}
+function checkPhoneNumber(){
+  const validNumber = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im; 
+  if (phone.validity.valueMissing){
+    phoneError.textContent = '*Please enter your phone number';
+  } else if(!phone.value.match(validNumber)){
+    phoneError.textContent = '*Format: (123) 456-7890';
+  } else {
+    phoneError.textContent = '';
   }
 }
